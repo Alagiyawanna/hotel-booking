488375a422a04c15a16465b77d5e7ec5b6b0b571
kAlagiyawanna <alagiyawanna204@gmail.com>
2025-03-31 00:16:39 +0530

    Refactor Ansible playbook for hotel booking application deployment, update Docker configurations, and modify Terraform settings for improved resource management

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,46 +1,9 @@
 provider "aws" {
-  region = "eu-north-1"
+  region = "us-east-1"
 }
 
 data "aws_instance" "hotel_booking" {
-  instance_id = "i-08eda6a6b8960c5ca"
-}
-
-resource "aws_security_group" "hotel_booking_sg" {
-  name        = "hotel-booking-sg"
-  description = "Security group for Hotel Booking application"
-
-  # SSH access
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Backend API access
-  ingress {
-    from_port   = 5000
-    to_port     = 5000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Frontend access
-  ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Allow all outbound traffic
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  instance_id = "i-016d05fdf5fd9623c"
 }
 
 resource "aws_network_interface_sg_attachment" "sg_attachment" {
