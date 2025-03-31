937b5443b559f7ed25f0fefc77101ddaa1132385
kAlagiyawanna <alagiyawanna204@gmail.com>
2025-03-31 22:02:13 +0530

    Update Docker Compose and config to use static IP for API URL

--- signin/src/config.js
+++ signin/src/config.js
@@ -1,7 +1,5 @@
 const config = {
-    API_URL: process.env.NODE_ENV === 'production' 
-      ? 'http://23.21.176.140:5000'
-      : 'http://localhost:5000'
-  };
-  
-  export default config;
+  API_URL: process.env.REACT_APP_API_URL || 'http://localhost:5000'
+};
+
+export default config;
